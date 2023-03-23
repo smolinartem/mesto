8abe621803a4e23352ef8dc82213dab6ff25d947
Artem Smolin <krutopognali@yandex.ru>
2023-03-23 17:18:52 +0300

    first review

--- js/index.js
+++ js/index.js
@@ -1,33 +1,29 @@
-const body = document?.body
-const btnEdit = body?.querySelector('.button_type_edit')
-const profileName = body?.querySelector('.profile__name')
-const profileJob = body?.querySelector('.profile__job')
+const btnEdit = document?.querySelector('.profile__button_type_edit')
+const profileName = document?.querySelector('.profile__name')
+const profileJob = document?.querySelector('.profile__job')
 
-const popupProfile = body?.querySelector('.popup')
+const popupProfile = document?.querySelector('.popup')
 const popupForm = popupProfile?.querySelector('.popup__form')
-const btnClose = popupProfile?.querySelector('.button_type_close')
+const btnClose = popupProfile?.querySelector('.popup__button_type_close')
 const inputName = popupProfile?.querySelector('.popup__input_type_name')
 const inputJob = popupProfile?.querySelector('.popup__input_type_job')
 
+const closePopup = () => {
+  popupProfile.classList.remove('popup_opened')
+}
+
 btnEdit.addEventListener('click', () => {
   popupProfile.classList.add('popup_opened')
-  body.classList.add('no-scroll')
   inputName.value = profileName.textContent
   inputJob.value = profileJob.textContent
 })
 
-btnClose.addEventListener('click', () => {
-  popupProfile.classList.remove('popup_opened')
-  body.classList.remove('no-scroll')
-})
+btnClose.addEventListener('click', closePopup)
 
 popupForm.addEventListener('submit', (event) => {
   event.preventDefault()
   profileName.textContent = inputName.value
   profileJob.textContent = inputJob.value
 
-  /*   console.log(isNaN(inputName.value)) */
-
-  popupProfile.classList.remove('popup_opened')
-  body.classList.remove('no-scroll')
+  closePopup()
 })
